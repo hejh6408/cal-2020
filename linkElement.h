4357c9f403f7f59aff15c7e91cb0a8b46d6a3544
hejh6408 <hejh6408@naver.com>
2019-12-16 05:38:20 +0900

    linkElement 에 대한 스마트 포인터 추가, 멤버 변수 추가.

--- linkElement.h
+++ linkElement.h
@@ -3,11 +3,22 @@
 #ifndef LINKELEMENT_H
 #define LINKELEMENT_H
 
+#include <memory>
+#include <vector>
+
+class linkElement;
+
+typedef std::shared_ptr<linkElement> pLinkElement;
+
+typedef  std::vector<pLinkElement> pLinkElementArray;
+
 class linkElement
 {
 public:
     linkElement();
     ~linkElement();
-};
 
+private:
+    pLinkElementArray m_linkElementArray;
+};
 #endif // LINKELEMENT_H
