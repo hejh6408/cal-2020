b4b61f383a68ef8ba83ba3f36a66918cdd44d01b
hejh6408 <hejh6408@naver.com>
2019-12-16 20:16:13 +0900

    test 함수 추가, 이 함수 내에서 간단한 기능 테스트를 진행

--- main.cpp
+++ main.cpp
@@ -2,10 +2,19 @@
 
 #include <QApplication>
 
+void test();
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+
+    test();
+
     return a.exec();
+}
+void test()
+{
+    // 여기서 자료 테스트 진행
 }
